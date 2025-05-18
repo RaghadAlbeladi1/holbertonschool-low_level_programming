719b72723524f6605ddd1d66dbe7d02670f0a439
RaghadAlbeladi1 <Raghadz_5@hotmail.com>
2025-05-18 05:49:37 +0000

    updated

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,9 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stddef.h>
+
+/* 0x05. C - Pointers, arrays and strings project prototypes */
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-int _strlen(char *s);
+size_t _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
@@ -14,5 +18,25 @@ char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 int _putchar(char c);
 
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
 
-#endif /*Bwave Bright*/
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+
+char *_memset(char *s, char b, unsigned int n);
+int _putchar(char c);
+#endif /* MAIN_H */
