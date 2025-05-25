416cb97d1a8f7aa99766120c0fa0c778c53944ac
RaghadAlbeladi1 <Raghadz_5@hotmail.com>
2025-05-25 10:44:42 +0000

    l

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -2,13 +2,13 @@
 #include "dog.h"
 
 /**
-* new_dog - creates a new dog
-* @name: name of dog
-* @age: age of dog
-* @owner: owner of dog
-*
-* Return: pointer to new dog
-*/
+ * new_dog - creates a new dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Return: pointer to new dog
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 unsigned int nl, ol, i;
